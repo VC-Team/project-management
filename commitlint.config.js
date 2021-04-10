77871a77d983534ed62b80a28f26d957d3f2ede8
Truong Nguyen <nguyenvantruong170797@gmail.com>
2021-04-10 17:40:19 +0700

    :sparkles: (commitlint) applies commitlint
    Closes #1

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,2 +1,10 @@
 // eslint-disable-next-line no-undef
-module.exports = { extends: ['gitmoji'] };
+module.exports = {
+	extends: ['gitmoji'],
+	parserPreset: {
+		parserOpts: {
+			headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
+			headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+		},
+	},
+};
