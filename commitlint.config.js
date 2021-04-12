59d6cf605f63f270e02e8e52483d4f4d4c6a0750
gialynguyen <emthanchet@gmail.com>
2021-04-12 14:38:41 +0700

    :recycle: refactor(pnpm): remove unused package
    Closes #1

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
 	extends: ['gitmoji'],
-	parserPreset: {
-		parserOpts: {
-			headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
-			headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
-		},
-	},
 };
