ff0e1a2d5ba2a9b161f7481ed88a2bb1d15c35b8
Truong Nguyen <nguyenvantruong170797@gmail.com>
2021-04-13 13:22:27 +0700

    :bug: fix(storybook-alias): config Storybook alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +9,19 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
 		'@whitespace/storybook-addon-html',
-  ]
+  ],
+	webpackFinal: (config) => {
+    config.resolve.modules.push(path.resolve(__dirname, "../src"));
+		config.resolve.alias = {
+      ...config.resolve.alias,
+			"@atoms": path.resolve(__dirname, "../src/components/atoms/"),
+      "@molecules": path.resolve(__dirname, "../src/components/molecules/"),
+      "@organisms": path.resolve(__dirname, "../src/components/organisms/"),
+      "@pages": path.resolve(__dirname, "../src/components/pages/"),
+      "@templates": path.resolve(__dirname, "../src/components/templates/"),
+      "@utils": path.resolve(__dirname, "../src/components/utils/"),
+    };
+		config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
 }
