954b1b420c2cecc390c91f8b1c2af163ca847a96
gialynguyen <emthanchet@gmail.com>
2021-04-21 21:43:39 +0700

    :sparkles: feat(config-source): config global style for vite + storybook
    Closes #39

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,35 +3,33 @@ import StorybookReactRouter from '../src/helpers/storybook-router';
 localStorage.removeItem('storybook-layout');
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  viewport: {
-    viewports: {
-      smallmobile: {
-        name: 'Mobile',
-        styles: {
-          width: '375px',
-          height: '576px',
-        },
-      },
-      tablet: {
-        name: 'Tablet',
-        styles: {
-          width: '577px',
-          height: '1199px',
-        },
-      },
-    },
-  },
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	viewport: {
+		viewports: {
+			smallmobile: {
+				name: 'Mobile',
+				styles: {
+					width: '375px',
+					height: '576px',
+				},
+			},
+			tablet: {
+				name: 'Tablet',
+				styles: {
+					width: '577px',
+					height: '1199px',
+				},
+			},
+		},
+	},
 	html: {
-    prettier: {
+		prettier: {
 			tabWidth: 2,
-      singleQuote: true,
-      bracketSpacing: true,
-      htmlWhitespaceSensitivity: 'ignore',
-    },
-  },
+			singleQuote: true,
+			bracketSpacing: true,
+			htmlWhitespaceSensitivity: 'ignore',
+		},
+	},
 };
 
-export const decorators = [
-  StorybookReactRouter,
-]
+export const decorators = [StorybookReactRouter];
