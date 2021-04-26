c064afde98fc0523e314fbf9e0eada997dfa87ae
Thái Hoàng Anh <54432722+thaihoanganh@users.noreply.github.com>
2021-04-26 15:31:24 +0700

    ♻️ refactor(tabs): remove context

--- src/components/organisms/tabs/context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export interface ITabsContext {
-	tabIndex: number | undefined;
-	onChangeTabIndex: (index: number) => void;
-}
-
-export const TabsContext = createContext({} as ITabsContext);
